c689894f964c18f27e7c010a83e24a38a62c117f
sofiabaezzato <122732986+sofiabaezzato@users.noreply.github.com>
2025-11-15 17:41:57 +0100

    style: home screen styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,15 @@
 import { Link } from "react-router";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
 	return (
-		<div className="min-h-screen bg-gradient-to-tr from-white via-pink-500 to-orange-500 flex items-center justify-center">
-			<div className="text-center text-white">
+		<div className="min-h-screen flex items-center">
+			<div className="text-center text-neutral-800 w-full mx-10 my-auto px-8 py-12 rounded-lg bg-orange-100">
 				<h1 className="text-6xl font-bold mb-4">Who Sings?</h1>
 				<p className="text-xl mb-8">Test your music knowledge</p>
-				<Link
-					to="/quiz"
-					className="bg-white text-purple-600 px-8 py-4 rounded-lg text-lg font-semibold hover:bg-gray-100 transition-colors"
-				>
-					Start Quiz
-				</Link>
+				<Button variant="default" className="text-lg font-semibold px-10 py-6">
+					<Link to="/quiz">Start Quiz</Link>
+				</Button>
 			</div>
 		</div>
 	);
