a8a93edbc3c9a8fd4bc9c07e679d33be2f179fcb
sofiabaezzato <122732986+sofiabaezzato@users.noreply.github.com>
2025-11-15 11:03:09 +0100

    chore: add vite proxy to prevent CORS errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,42 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import type { IncomingMessage } from "http";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
+	const PORT = `${env.VITE_PORT ?? "3000"}`;
+	const API_KEY = `${env.VITE_API_KEY ?? ""}`;
+
+	return {
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
 		},
-	},
+		server: {
+			port: parseInt(PORT),
+			proxy: {
+				"/api": {
+					target: "https://api.musixmatch.com",
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ""),
+					configure: (proxy, _options) => {
+						proxy.on(
+							"proxyReq",
+							(proxyReq: any, req: IncomingMessage, _res: any) => {
+								const url = new URL(req.url!, "http://localhost");
+								url.searchParams.set("apikey", API_KEY);
+								proxyReq.path = url.pathname + url.search;
+								console.log(proxyReq.path);
+							},
+						);
+					},
+				},
+			},
+		},
+	};
 });
